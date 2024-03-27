1b88e6ad42df174e070b3b407fb3fda418f4c6d2
Kailash1233 <kailash61203@gmail.com>
2024-03-28 00:20:10 +0530

    minor commit

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 function Footer() {
   return (
     <footer>
-      <p>From player to player</p>
+      {/* <p>From player to player</p> */}
       <h4>Customized by Kailash G</h4>
     </footer>
   );
